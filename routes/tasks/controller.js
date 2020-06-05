8503461664e112d93e783e7087125b2ffb33ac62
targhib <targhib.ibrahim49@gmail.com>
2020-06-05 22:36:58 +0700

    add delete function on tasks

--- routes/tasks/controller.js
+++ routes/tasks/controller.js
@@ -57,120 +57,16 @@ module.exports = {
 		} catch (error) {
 			throw error;
 		}
+	},
+	deleteTask: async (req, res) => {
+		try {
+			const { id } = req.params;
+
+			const deletedTask = await Tasks.findByIdAndDelete(id);
+
+			res.status(200).json({ message: 'Task Status Updated', data: updatedTask });
+		} catch (error) {
+			console.log(error);
+		}
 	}
-	// getUserById: async (req, res) => {
-	// 	try {
-	// 		const id = req.user._id;
-	// 		const status = req.user.isLoggedIn;
-	// 		const user = await User.findById(id);
-	// 		res.status(200).json({ currentUser: user, isLoggedIn: status });
-	// 	} catch (error) {
-	// 		console.log(error);
-	// 	}
-	// },
-	// registration: async (req, res) => {
-	// 	try {
-	// 		const { prefix, fullname, email, hp, password, role, status } = req.body;
-
-	// 		// Check if email has already registered
-	// 		const result = await User.findOne({ email: email });
-	// 		if (result) return res.status(401).send('Your email has already registered');
-
-	// 		bcrypt.genSalt(10, function(err, salt) {
-	// 			bcrypt.hash(password, salt, async function(err, hash) {
-	// 				const users = await User.create({
-	// 					prefix,
-	// 					fullname,
-	// 					email,
-	// 					hp,
-	// 					password: hash,
-	// 					role,
-	// 					status
-	// 				});
-	// 				res.status(201).send({
-	// 					message: 'New user successfully created!',
-	// 					data: users
-	// 				});
-	// 			});
-	// 		});
-	// 	} catch (error) {
-	// 		console.log(error);
-	// 	}
-	// },
-	// login: async (req, res) => {
-	// 	const { email, password } = req.body;
-	// 	const result = await User.findOne({ email: email });
-
-	// 	// Check if email exist
-	// 	if (!result) return res.status(401).send('Your email is not registered');
-
-	// 	const { _id, status } = result;
-
-	// 	// Check status
-	// 	if (status === 'pending') return res.status(401).send('Your account is not activated yet');
-
-	// 	// Check if email and password match
-	// 	bcrypt.compare(password, result.password).then((response) => {
-	// 		if (response === true) {
-	// 			const token = jwt.sign({ _id, isLoggedIn: true }, SECRET_KEY, {
-	// 				expiresIn: '15m'
-	// 			});
-	// 			const refreshToken = jwt.sign({ _id, isLoggedIn: true }, REFRESH_KEY, { expiresIn: '24h' });
-	// 			res.status(200).send({ token: token, refreshToken: refreshToken });
-	// 		} else {
-	// 			res.status(401).send("Your email and password don't match");
-	// 		}
-	// 	});
-	// },
-	// refreshToken: async (req, res) => {
-	// 	const { refreshToken } = req.body;
-	// 	const result = await User.findOne({ token: refreshToken });
-	// 	if (!result) return res.status(401).send('Forbidden');
-	// 	if (refreshToken !== result.token) return res.status(403).send('Invalid Credentials');
-	// 	jwt.verify(refreshToken, REFRESH_KEY, (err, data) => {
-	// 		if (err) return res.status(403).send('Invalid Credentials');
-	// 		const accessToken = jwt.sign({ _id: data._id, isLoggedIn: true }, SECRET_KEY, { expiresIn: '15m' });
-	// 		res.status(200).send({ token: accessToken });
-	// 	});
-	// },
-	// addToken: async (req, res) => {
-	// 	try {
-	// 		const { id } = req.params;
-	// 		const { refreshToken } = req.body;
-
-	// 		const result = await User.findByIdAndUpdate(
-	// 			id,
-	// 			{
-	// 				$set: {
-	// 					token: refreshToken
-	// 				}
-	// 			},
-	// 			{ new: true }
-	// 		);
-
-	// 		res.status(200).json({
-	// 			message: 'Add token success',
-	// 			data: result
-	// 		});
-	// 	} catch (error) {
-	// 		console.log(error);
-	// 	}
-	// },
-	// logout: async (req, res) => {
-	// 	const { id } = req.params;
-	// 	const result = await User.findByIdAndUpdate(
-	// 		id,
-	// 		{
-	// 			$set: {
-	// 				token: ''
-	// 			}
-	// 		},
-	// 		{ new: true }
-	// 	);
-
-	// 	res.status(200).json({
-	// 		message: 'Delete token success',
-	// 		data: result
-	// 	});
-	// }
 };
--- routes/tasks/index.js
+++ routes/tasks/index.js
@@ -2,18 +2,12 @@ const express = require('express');
 const router = express.Router();
 const verify = require('../authorization');
 
-const {
-	getAll,
-	getTaskByUserId,
-	createNewTask,
-	updateStatus
-	// deleteTask
-} = require('./controller');
+const { getAll, getTaskByUserId, createNewTask, updateStatus, deleteTask } = require('./controller');
 
 router.get('/', getAll);
 router.get('/id', verify, getTaskByUserId);
 router.post('/', createNewTask);
 router.put('/:id', updateStatus);
-// router.delete('/token', deleteTask);
+router.delete('/', deleteTask);
 
 module.exports = router;
